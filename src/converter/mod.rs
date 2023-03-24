fac7b08ec4cce49ed01d05ab5f39dcc2b96d81ca
Martin <martinpfatrisch@gmail.com>
2023-03-24 16:07:00 +0100

    Validation of input color code and detection of format of input color code

--- src/converter/mod.rs
+++ src/converter/mod.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq)]
+#[derive(Clone, Copy, PartialEq)]
 enum ColorFormat {
     Hex,
     Rgb,
@@ -6,14 +6,19 @@ enum ColorFormat {
 
 pub struct Converter {
     input_color_code: String,
+    input_format: ColorFormat,
     output_format: ColorFormat,
 }
 
 impl Converter {
     pub fn new(input_color_code: String, is_hex: bool, is_rgb: bool) -> Result<Converter, String> {
         let color_code: String;
+        let input_format: ColorFormat;
         match Converter::validate_color_code(input_color_code) {
-            Ok(c) => color_code = c,
+            Ok((c, f)) => {
+                color_code = c;
+                input_format = f;
+            }
             Err(err) => return Err(err),
         }
 
@@ -25,16 +30,29 @@ impl Converter {
 
         Ok(Converter {
             input_color_code: color_code,
+            input_format: input_format,
             output_format: output_format,
         })
     }
 
-    fn validate_color_code(color_code: String) -> Result<String, String> {
+    fn validate_color_code(color_code: String) -> Result<(String, ColorFormat), String> {
+        let input_format: ColorFormat;
+
         if color_code.contains('.') {
             return Err(String::from("Not a valid color code"));
         }
 
-        Ok(color_code)
+        if color_code.contains(',') {
+            input_format = ColorFormat::Rgb;
+        } else if color_code.starts_with('#') {
+            input_format = ColorFormat::Hex;
+        } else {
+            return Err(String::from(
+                "Couldn't determine the format of the input color code",
+            ));
+        }
+
+        Ok((color_code, input_format))
     }
 
     fn validate_format(is_hex: bool, is_rgb: bool) -> Result<ColorFormat, String> {
@@ -55,17 +73,23 @@ impl Converter {
     }
 
     pub fn print_info(&self) {
-        let mut format = String::new();
+        let input_format = Converter::get_format(self.input_format);
+        let output_format = Converter::get_format(self.output_format);
 
-        if self.output_format == ColorFormat::Hex {
+        println!(
+            "\nThe color {} is of format {} and will be converted to {} format.\n",
+            self.input_color_code, input_format, output_format
+        )
+    }
+
+    fn get_format(color_format: ColorFormat) -> String {
+        let mut format = String::new();
+        if color_format == ColorFormat::Hex {
             format = String::from("hexadecimal");
-        } else if self.output_format == ColorFormat::Rgb {
+        } else if color_format == ColorFormat::Rgb {
             format = String::from("RGB")
         }
 
-        println!(
-            "\nThe color {}, will be converted to {} format.\n",
-            self.input_color_code, format
-        )
+        format
     }
 }
