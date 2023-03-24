26f7e2f482238c844475e1a7cde87f7c068df176
Martin <martinpfatrisch@gmail.com>
2023-03-24 09:14:39 +0100

    Created first draft of the Cli input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use clap::Parser;
+
+mod cli;
 mod converter;
 
 fn main() {
-    let converter = converter::Converter::new(String::from("Martin"));
+    let args = cli::Cli::parse();
+
+    let converter = converter::Converter::new(args.name);
 
     converter.greet()
 }
